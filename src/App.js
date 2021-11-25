f15f2bf504247f492352436bc3db9c0c7902d31a
JessicaAov1 <86833978+JessicaAov1@users.noreply.github.com>
2021-11-25 22:08:35 +0000

    Added a link to the footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,18 @@ function App() {
           <Dictionary />
         </main>
       </div>
-      <footer className="App-footer">Coded by Jessica</footer>
+      <footer className="App-footer">
+        <em> Open-source code by </em>
+        <a
+          href="https://github.com/JessicaAov1/react-dictionary"
+          className="link-Git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jessica Aronov
+        </a>
+        and hosted by Netlify
+      </footer>
     </div>
   );
 }
